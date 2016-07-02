cb4979c4f294181c9ac2fd180327a4f74ee97740
UIman <alexvikev@gmail.com>
2016-07-03 02:39:17 +0300

    First update

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+''' Файл методов ответа на запрос пользователя '''
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
