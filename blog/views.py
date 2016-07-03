779ac112789a72ad739002b2b212a415affd6fd7
UIman <alexvikev@gmail.com>
2016-07-04 01:44:25 +0300

    Modified site

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,19 @@
 ''' Файл методов ответа на запрос пользователя '''
 ''' Соединяют шаблоны и модели: берут модели и передают их в шаблон '''
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
+    ''' Функция представления списка записей '''
 
     # сортировка по дате
     # render возарвщает список
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    ''' Функция детального представления записи '''
+
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
