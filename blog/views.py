2985ab6c5548a09b71dbad95fdf09a98d7bd5391
UIman <alexvikev@gmail.com>
2016-07-03 19:03:25 +0300

    Modified templates

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 ''' Файл методов ответа на запрос пользователя '''
+''' Соединяют шаблоны и модели: берут модели и передают их в шаблон '''
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+
+    # сортировка по дате
+    # render возарвщает список
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
